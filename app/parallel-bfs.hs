55a2c8fe21cfe083e37125c4a63f665ae3913cf8
Ryan Wee <rw2800@columbia.edu>
2023-12-20 01:51:53 -0500

    Don't include I/O in timing

--- app/parallel-bfs.hs
+++ app/parallel-bfs.hs
@@ -8,7 +8,9 @@ import Lib (
 
 import qualified Data.Map as Map
 
+-- import Control.DeepSeq (deepseq, NFData)
 import Control.Monad.Par (runPar)
+import System.CPUTime (getCPUTime)
 
 {- ******** START DATA FILES ******** -}
 
@@ -23,10 +25,32 @@ queriesFile = "data/wikigraph.queries"
 
 {- ******** END DATA FILES ******** -}
 
+-- forceEvaluationOfMap :: (NFData k, NFData v) => Map.Map k v -> ()
+-- forceEvaluationOfMap m = Map.foldlWithKey (\_ k v -> k `deepseq` v `deepseq` ()) () m
+
+-- forceEvaluationOfList :: (NFData a) => [a] -> ()
+-- forceEvaluationOfList l = foldl (\_ x -> x `deepseq` ()) () l
+
 main :: IO ()
 main = do
     graph <- readBinaryFileToGraph edgesFile
     nodes <- readJSONFileToNodes nodesFile
-    putStrLn "Loaded " ++ (show $ Map.size nodes) ++ " Nodes"
     queries <- readCSVFileToQueries queriesFile nodes
+
+    -- Force the full evaluation of graph / nodes / queries, so that we don't
+    -- include I/O in our timing results.
+    --
+    -- TODO: Don't rely on Map.size / length, which might not fully evaluate the
+    --       data structures.
+    putStrLn $ "Loaded " ++ (show $ Map.size graph) ++ " Edges"
+    putStrLn $ "Loaded " ++ (show $ Map.size nodes) ++ " Nodes"
+    putStrLn $ "Loaded " ++ (show $ length queries) ++ " Queries"
+    -- let _ = (forceEvaluationOfMap graph) `deepseq` ()
+    -- let _ = (forceEvaluationOfMap nodes) `deepseq` ()
+    -- let _ = (forceEvaluationOfList queries) `deepseq` ()
+
+    start <- getCPUTime
     putStrLn $ show $ runPar $ runQueries graph queries
+    end <- getCPUTime
+    let runtime = (end - start) `div` 1000000 -- us
+    putStrLn $ "Runtime: " ++ show runtime ++ " us"
